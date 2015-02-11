8f326555bde0aac54270a75cd2c5aad241039ea1
Satoshi OGA <tiger.taiga.taiga@gmail.com>
2015-02-11 23:49:33 +0900

    リソース名変更．バカラをビューにして強制的にデバッグ中．

--- source/CasinoPrototype/src/Baccarat/Baccarat.java
+++ source/CasinoPrototype/src/Baccarat/Baccarat.java
@@ -6,9 +6,6 @@ import ademang.third.CasinoPrototype.*;
 //現在は上辺だけのバカラ．
 //API use debuging now
 import java.util.Random;
-import java.util.ResourceBundle;
-
-import android.R.integer;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -20,7 +17,12 @@ import android.graphics.Rect;
 import android.view.MotionEvent;
 import android.view.View;
 
-public class Baccarat implements SceneValues,Games{
+public class Baccarat 
+//implements SceneValues,Games{
+	extends View{
+	
+	
+	
 	private Canvas canvas;
 	private int	scene;
 	private Random rand;
@@ -53,22 +55,18 @@ public class Baccarat implements SceneValues,Games{
 	
 	//以下アンドロイドAPI
 	private Paint paint;
-	
 	private Bitmap[] bitmapTramp;	//トランプビットマップ
-
 	private Bitmap[] parsageWinnerBmp; //勝者の選択画像
 	
-	//テスト用
-	private Bitmap tester; //凛ちゃんの顔
-	private int count;
 	
 		
 	
 	//コンストラクタ
-	public Baccarat(Canvas canvas,Resources r) {
-		this.canvas = canvas;
-		this.loadResource(r);
-		
+//	public Baccarat(Canvas canvas,Resources r) {
+	public Baccarat(Context context){
+		super(context);
+//		this.canvas = canvas;
+//		this.loadResource(r);
 		trumpSet();
 		
 		playerTrump = new byte[3];
@@ -80,7 +78,6 @@ public class Baccarat implements SceneValues,Games{
 		winnerPresage = 'n';
 		sceneSerch = parsagePush;
 		
-		
 		paint = new Paint();
 		paint.setColor(Color.BLACK);
 		paint.setTextSize(25);
@@ -89,27 +86,30 @@ public class Baccarat implements SceneValues,Games{
 		bitmapTramp = new Bitmap[trampNum+1];
 		parsageWinnerBmp = new Bitmap[3];
 
-		count=0;
 //		loadResource();
+		loadResource(getResources());
+
 		// TODO 自動生成されたコンストラクター・スタブ
 	}
 	
 	private void loadResource(Resources r){
-		/*
-		int bitmapID = 0x7f020000;
-		for(int i=0; i<=trampNum; i++)
-			bitmapTramp[i] =  BitmapFactory.decodeResource(getResources(), bitmapID++);
-		*/
-		for(int i=0; i<53; i++){
-			
+
+		bitmapTramp[0] = BitmapFactory.decodeResource(r,R.drawable.trump1back);
+		for(int i=1; i<=13; i++){
+			int resId1 = r.getIdentifier("trump1spade"+String.valueOf(i),"drawable","ademang.third.CasinoPrototype");
+			int resId2 = r.getIdentifier("trump1heart"+String.valueOf(i),"drawable","ademang.third.CasinoPrototype");
+			int resId3 = r.getIdentifier("trump1dia"+String.valueOf(i),"drawable","ademang.third.CasinoPrototype");
+			int resId4 = r.getIdentifier("trump1club"+String.valueOf(i),"drawable","ademang.third.CasinoPrototype");
+			bitmapTramp[i] = BitmapFactory.decodeResource(r,resId1);
+			bitmapTramp[i+13] = BitmapFactory.decodeResource(r, resId2);
+			bitmapTramp[i+26] = BitmapFactory.decodeResource(r, resId3);
+			bitmapTramp[i+39] = BitmapFactory.decodeResource(r, resId4);
 		}
-			
-		
 		
 		//勝者の選択
-//		parsageWinnerBmp[0] = BitmapFactory.decodeResource(getResources(), R.drawable.e_button_player);		
-//		parsageWinnerBmp[1] = BitmapFactory.decodeResource(getResources(), R.drawable.e_button_banker);
-//		parsageWinnerBmp[2] = BitmapFactory.decodeResource(getResources(), R.drawable.e_button_tie);		
+		parsageWinnerBmp[0] = BitmapFactory.decodeResource(r, R.drawable.e_button_player);		
+		parsageWinnerBmp[1] = BitmapFactory.decodeResource(r, R.drawable.e_button_banker);
+		parsageWinnerBmp[2] = BitmapFactory.decodeResource(r, R.drawable.e_button_tie);		
 	}
 	
 	public void killResource(){
@@ -295,19 +295,42 @@ public class Baccarat implements SceneValues,Games{
 		return 0;
 	}
 
+	
+	public void animation(){
+		
+	}
+
+	//トランプを配る
+	public void trumpDistribute(){
+		distribute();
+		if(natural())
+			win = winner();
+		else{
+			if(playerDraw() == true){
+				playerTrump[2] = draw();
+			}
+			if(bankerDraw() == true){
+				bankerTrump[2] = draw();
+			}
+			playerTrumpNum = sumTrump(playerTrump);
+			bankerTrumpNum = sumTrump(bankerTrump);
+			win = winner();
+		}
+
+	}
 
 	/*
 	 * 実行部分
 	 * drawText use Debug
 	 */
 	
-	public void draw(Canvas canvas){
+//	public void draw(Canvas canvas){
+	@Override
+	public void onDraw(Canvas canvas){
 		//描画領域の最大値を求める
 		int w = canvas.getWidth();
 		int h = canvas.getHeight();
 
-		
-		
 		//勝者予測選択ボタンのRECT変数
 		Rect[] parsageWinBmpDst = new Rect[3];
 		Rect parsageWinBmpSrc = new Rect(0,0,parsageWinnerBmp[0].getWidth(),parsageWinnerBmp[0].getHeight());
@@ -315,20 +338,22 @@ public class Baccarat implements SceneValues,Games{
 		
 		//予測ボタンのRECTを書き換える
 		for(int i=0; i<parsageWinnerBmp.length; i++){
-			parsageWinBmpDst[i] = new Rect(0,0,parsageWinnerBmp[i].getWidth()+w/2,parsageWinnerBmp[i].getHeight()+h/12); 
-			parsageWinBmpDst[i].offsetTo(w/7, h/4*(i)+50);
+//			parsageWinBmpDst[i] = new Rect(0,0,parsageWinnerBmp[i].getWidth()+w/2,parsageWinnerBmp[i].getHeight()+h/12); 
+			parsageWinBmpDst[i] = new Rect(0,0, w/90/2*parsageWinnerBmp[i].getWidth(), h/160/2*parsageWinnerBmp[i].getHeight());
+			parsageWinBmpDst[i].offsetTo((w/2)-(parsageWinBmpDst[i].width()/2), h/4*(i)+50);
 		}
 		
 		//トランプのRECTを書き換える
 		Rect[] playerTrumpDst = new Rect[3];
 		Rect[] bankerTrumpDst = new Rect[3];
 		Rect trumpSrc = new Rect(0,0,bitmapTramp[0].getWidth(),bitmapTramp[0].getHeight());
+
 		for(int i=0; i<3; i++){
 			playerTrumpDst[i] = new Rect(0,0,bitmapTramp[0].getWidth(),bitmapTramp[0].getHeight());
 			bankerTrumpDst[i] = new Rect(0,0,bitmapTramp[0].getWidth(),bitmapTramp[0].getHeight());
 			//トランプの描画ポジションを設定
 			playerTrumpDst[i].offsetTo(20+i*100, 20);
-			bankerTrumpDst[i].offsetTo(20+i*100, 400); 
+			bankerTrumpDst[i].offsetTo(20+i*100, 400);
 		}
 		
 		//勝者予測選択画面の表示
@@ -337,46 +362,52 @@ public class Baccarat implements SceneValues,Games{
 				canvas.drawBitmap(parsageWinnerBmp[i], parsageWinBmpSrc, parsageWinBmpDst[i],paint);
 		}
 		
+		//掛け金の変更
+		else if(sceneSerch == manyBets){
+			
+		}
+		
 		//ゲームの結果の表示
 		else if(sceneSerch == resultGame){
 			//ゲームの結果画面．
-			if(canNextGame()){			
-				distribute();
+			//もう一度引くことができるよ．
+			if(canNextGame()){
+				trumpDistribute();
+				
+				//以下トランプの表示
 				for(int i=0; i<2; i++){
 					canvas.drawBitmap(bitmapTramp[playerTrump[i]],trumpSrc,playerTrumpDst[i],paint);
 					canvas.drawBitmap(bitmapTramp[bankerTrump[i]],trumpSrc,bankerTrumpDst[i],paint);
 				}
+//				canvas.drawBitmap(bitmapTramp[playerTrump[2]],trumpSrc,playerTrumpDst[2],paint);
+//				canvas.drawBitmap(bitmapTramp[bankerTrump[2]],trumpSrc,bankerTrumpDst[2],paint);
+
 				
 				playerTrumpNum = sumTrump(playerTrump);
 				bankerTrumpNum = sumTrump(bankerTrump);
-				if(natural())
-					win = winner();
-				else{
-					if(playerDraw() == true){
-						playerTrump[2] = draw();
-						canvas.drawBitmap(bitmapTramp[playerTrump[2]],trumpSrc,playerTrumpDst[2],paint);
-		
-					}
-					if(bankerDraw() == true){
-						bankerTrump[2] = draw();
-						canvas.drawBitmap(bitmapTramp[bankerTrump[2]],trumpSrc,bankerTrumpDst[2],paint);
-					}
-					playerTrumpNum = sumTrump(playerTrump);
-					bankerTrumpNum = sumTrump(bankerTrump);
-					win = winner();
-				}
-				
+
 				//デバッグ用
 				canvas.drawText("p === > "+ playerTrumpNum, 50, 650, paint);
 				canvas.drawText("b === > " + bankerTrumpNum, 50, 675, paint);
 				canvas.drawText("useTramp = > " + trumpCount, 50, 700, paint);
 				canvas.drawText("Winner == " + win, 50, 725, paint);
-				canvas.drawBitmap(tester,300,600,paint);
 			}
-		}			
+		}
+		 
+		//その他
+		else{
+			//もう一度やるけど，もうトランプがない場合．
+			if(!(canNextGame())){
+				
+			}
+			
+		}
 	}
 	
-	public void processTouchEvent(MotionEvent event){
+	
+//	public void processTouchEvent(MotionEvent event){
+	@Override
+	public boolean onTouchEvent(MotionEvent event){
 		int tX = (int)event.getX();		//タッチされたところのX軸を取得
 		int tY = (int)event.getY();		//		〃           Y軸を取得
 
@@ -400,6 +431,7 @@ public class Baccarat implements SceneValues,Games{
 		else {
 			
 		}
+		return false;
 	}
 
 }
